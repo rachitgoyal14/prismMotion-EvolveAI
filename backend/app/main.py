bdf791078c93bf295776f74d848cd416d230ca0d
rachitgoyal14 <goyalrachit1410@gmail.com>
2026-02-08 05:00:59 +0530

    rebase abort

--- backend/app/main.py
+++ backend/app/main.py
@@ -38,8 +38,8 @@ import io
 from app.utils.logging_config import setup_logging, StageLogger
 from app.utils.video_utils import convert_to_portrait_9_16
 
-from app.utils.video_utils import convert_to_portrait_9_16
-from app.utils.video_utils import convert_to_portrait_9_16
+# from app.utils.video_utils import convert_to_portrait_9_16
+# from app.utils.video_utils import convert_to_portrait_9_16
 
 from app.chat.routes import router as chat_router
 setup_logging(level=logging.INFO)
